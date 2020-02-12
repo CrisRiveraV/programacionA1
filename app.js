f7f0c70d38c2bd4e58083c66a340e869b5d48df2
usis023618 <crisrivera.172@gmail.com>
2020-02-12 10:27:17 -0600

    usis023618

---
+++ app.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", (event)=>{
+    const formAlumnos = document.querySelector("#frmAlumnos");
+formAlumnos.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let codigo = document.querySelector("#txtCodigoAlumno").value,
+        nonbre = document.querySelector("#txtNombreAlumno").value,
+        direccion = document.querySelector("#txtDireccionAlumno").value,
+        telefono = document.querySelector("#txtTelefonoAlumno").value;
+    if('localStorage' in window ){
+        window.localStorage.setItem("codigo", codigo);
+        window.localStorage.setItem("nombre", nombre);
+        window.localStorage.setItem("direccion", direccion);
+        window.localStorage.setItem("telefono", telefono);
+    }else{
+        alert('almacenamiento en local NO soportado!!! Actualizate!');
+    }
+});
+document.querySelector("#btnRecuperarAlumnos").addEventListener("click", (e)=>{
+    if('localStorage' in window){
+        document.querySelector("#txtCodigoAlumno").value =
+        window.localStorage.getItem("codigo");
+        document.querySelector("#txtNombreAlumno").value =
+        window.localStorage.getItem("codigo");
+        document.querySelector("#txtTelefonoAlumno").value =
+        window.localStorage.getItem("codigo");
+        document.querySelector("#txtDireccionAlumno").value =
+        window.localStorage.getItem("codigo");
+        
+    }
+});
+});
