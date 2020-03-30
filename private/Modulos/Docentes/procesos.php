1872282428159df49a9b2ad2142180586d8b49e5
usis023618 <crisrivera.172@gmail.com>
2020-03-29 21:59:25 -0600

    cambios

--- private/Modulos/Docentes/procesos.php
+++ private/Modulos/Docentes/procesos.php
@@ -43,7 +43,7 @@ class docente{
         if( $this->respuesta['msg']==='correcto'){
             if($this->datos['accion']==='nuevo'){
                 $this->db->consultas('
-                INSERT INTO docentes (codigo, nombre, direccion, telefono) VALUES(
+                INSERT INTO docentes (codigo, nombre, nit, direccion, telefono) VALUES(
                     "'. $this->datos['codigo'] .'",
                     "'. $this->datos['nombre'] .'",
                     "'. $this->datos['nit'] .'",
